d74c1f6fcf4f72bcf7141a7bada8a5e95789cb63
Rayavitor <120482293+Rayavitor@users.noreply.github.com>
2024-09-04 19:26:06 -0300

    Update script.js

--- cef/script.js
+++ cef/script.js
@@ -1,26 +1,49 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const dialogOverlay = document.getElementById('dialog-overlay');
-    const openDialogButton = document.getElementById('open-dialog');
-    const closeDialogButton = document.getElementById('close-dialog');
-    const yesButton = document.getElementById('yes-button');
-    const noButton = document.getElementById('no-button');
+// Função para criar e exibir notificações
+function Alert(type, title, message, time) {
+    // Cores e ícones para os tipos de notificação
+    var icons = {
+        "geral" : "fas fa-keyboard",
+        "info" : "fas fa-info-circle",
+        "sucesso" : "fas fa-check-circle",
+        "erro" : "fas fa-server",
+        "aviso" : "fas fa-exclamation-triangle",
+        "sms" : "fas fa-sms"
+    };
 
-    function showDialog() {
-        dialogOverlay.classList.remove('hidden');
-    }
+    var colours = {
+        "geral" : "#95a5a6",
+        "info" : "#3498db",
+        "sucesso" : "#1abc9c",
+        "erro" : "#dc3545",
+        "aviso" : "#f1c40f",
+        "sms" : "#e67e22"
+    };
 
-    function hideDialog() {
-        dialogOverlay.classList.add('hidden');
-    }
+    // Gerar um identificador único para a notificação
+    var number = Math.floor((Math.random() * 1000) + 1);
 
-    openDialogButton.addEventListener('click', showDialog);
-    closeDialogButton.addEventListener('click', hideDialog);
-    yesButton.addEventListener('click', () => {
-        alert('You clicked Yes!');
-        hideDialog();
-    });
-    noButton.addEventListener('click', () => {
-        alert('You clicked No!');
-        hideDialog();
-    });
+    // Adicionar a notificação ao DOM
+    $('.notify-wrapper').append(`
+        <div class="notify-div wrapper-${number}" style="border-left: 5px solid ${colours[type]}; display:none">
+        <div class="align-items-baseline notify-title">
+            <i class="${icons[type]} fa-ms notify-icon" style="color: ${colours[type]}"></i>
+            <h5 class="text-uppercase notify-title-text" style="color: ${colours[type]}">${title}</h5>
+        </div>
+        <p class="text-break notify-main-text">${message}</p>
+        </div>`);
+
+    // Exibir a notificação
+    $(`.wrapper-${number}`).fadeIn("slow");
+
+    // Ocultar e remover a notificação após um tempo
+    setTimeout(function () {
+        $(`.wrapper-${number}`).fadeOut("slow", function () {
+            $(`.wrapper-${number}`).remove();
+        });
+    }, time);
+}
+
+// Configuração do evento de notificação do CEF
+cef.on("send:action:notificar", (type, title, message, time) => {
+    Alert(type, title, message, time);
 });
