52443d905697b5ca33b867813ef5be179394d039
Rayavitor <120482293+Rayavitor@users.noreply.github.com>
2025-06-04 20:11:08 -0300

    Delete segments directory

--- segments/segments_map_off_HIPEX_81dc9bdb52d04dc20036dbd8313ed055_1748105499.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  '6831f7fd7bb7f' => 'https://hls.chora-embedflix.shop/off_1748073299_3285.ts',
-  '6831f7fd7bf67' => 'https://hls.chora-embedflix.shop/off_1748073299_3286.ts',
-  '6831f7fd7c34f' => 'https://hls.chora-embedflix.shop/off_1748073299_3287.ts',
-  '6831f7fd7c737' => 'https://hls.chora-embedflix.shop/off_1748073299_3288.ts',
-  '6831f7fd7cb1f' => 'https://hls.chora-embedflix.shop/off_1748073299_3289.ts',
-  '6831f7fd7cf07' => 'https://hls.chora-embedflix.shop/off_1748073299_3290.ts',
-);
